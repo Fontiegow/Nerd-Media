762e4cda73bd9d0202c905d902e5a00bb2b8e72f
Fontiegow <businessboyfarid@gmail.com>
2025-12-19 23:52:13 +0330

    Working on steam data Stage 1.

--- steam-data/src/steam/config.py
+++ steam-data/src/steam/config.py
@@ -1,5 +1,22 @@
-# src/steam/config.py
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-STEAM_APP_LIST_URL = "https://api.steampowered.com/IStoreService/GetAppList/v1/"
-STEAM_API_KEY = None   # از env می‌گیریم
+# Project Path
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+DATA_RAW = BASE_DIR / "data/raw"
+
+# Path to .env file
+ENV_PATH = BASE_DIR / ".env"
+load_dotenv(dotenv_path=ENV_PATH)
+
+# Make sure the raw data directory exists
+DATA_RAW.mkdir(parents=True, exist_ok=True)
+
+# API Configuration
+# STEAM_APP_LIST_URL = "https://api.steampowered.com/IStoreService/GetAppList/v1/"
+STEAM_APP_LIST_URL = "https://api.steampowered.com/ISteamApps/GetAppList/v2"
+
+# Recommended to use an API key for higher rate limits
+STEAM_API_KEY = os.getenv("STEAM_API_KEY") 
 USER_AGENT = "SteamDataCollector/0.1 (research)"
